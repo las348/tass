a0696d26d69cb637f1e6482fdab94c7648fe072c
Laureni Wilkinson <las348@nyu.edu>
2020-07-15 14:31:13 -0400

    initial table

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,15 +1,36 @@
 import React from "react";
 import Jumbotron from "./components/jumbotron";
 import Search from "./components/search-directory";
+import Table from "./components/table";
 
 
-function App() {
-  return (
-    <div className="container">
-      <Jumbotron />
-      <Search />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableData2: [
+        { 'Name': 'Abc', 'Age': 15, 'Location': 'Bangalore' },
+        { 'Name': 'Def', 'Age': 43, 'Location': 'Mumbai' },
+        { 'Name': 'Uff', 'Age': 30, 'Location': 'Chennai' },
+        { 'Name': 'Ammse', 'Age': 87, 'Location': 'Delhi' },
+        { 'Name': 'Yysse', 'Age': 28, 'Location': 'Hyderabad' }
+      ]
+    }
+  }
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <Jumbotron />
+        <Search />
+        <Table data={this.state.tableData2} />
+      </div>
+
+    );
+  }
+
+
 }
 
-export default App;
