79560b93547ecdf2c450c6ff768974612adad9e7
Laureni Wilkinson <las348@nyu.edu>
2020-07-26 19:59:37 -0400

    final

--- employee-directory/src/components/SearchResult.js
+++ employee-directory/src/components/SearchResult.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import "./../styles/table.css"
+
 
 function TableData(props) {
     const { users } = props;
-    const { handleSort } = props;
+
+    const message = () => {
+        alert("Sorry, sorting is not currently available")
+    }
 
     return (
         <div>
-            <div className="row justify-content-center">
+            <div className="row justify-content-center table">
                 <div className="col-md-8">
-                    <table className="table table-striped">
+                    <table className="table">
                         <thead >
                             <tr>
                                 <th>Picture</th>
-                                <th onClick={handleSort}>Name</th>
+                                <th onClick={message} id="name">Name</th>
                                 <th>Email</th>
                                 <th>Phone</th>
                                 <th>Age</th>
@@ -21,7 +26,7 @@ function TableData(props) {
                         <tbody>
                             {users.map((user, index) => (<tr key={index}>
                                 <td><img alt={user.name.first} src={user.picture.thumbnail} className="img-fluid" /></td>
-                                <td>{user.name.first} {user.name.last}</td>
+                                <td>{user.name.first} {user.name.last}</td>                              
                                 <td>{user.email}</td>
                                 <td>{user.phone}</td>
                                 <td>{user.dob.age}</td>
--- employee-directory/src/components/SearchResultContainer.js
+++ employee-directory/src/components/SearchResultContainer.js
@@ -3,11 +3,13 @@ import SearchResult from "./SearchResult";
 import SearchBar from "./SearchBar";
 import axios from "axios";
 
+
 function ResultContainer() {
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([]);
     const [filterOption, setFilterOption] = useState("");
     const [filteredState, setFilteredState] = useState([]);
-    const [sortState, setSortState] = useState([]);
+    // const [sortBy, setSortBy] = useState({key: null, order: '>' });
+   
 
     useEffect(() => {
         loadUsers();
@@ -17,96 +19,71 @@ function ResultContainer() {
         axios.get("https://randomuser.me/api/?results=20&nat=us")
             .then(res => {
                 const employee = res.data.results
+                console.log(employee);
                 setUsers(employee)
             });
     }
 
-    // function handleSort() {
-       
-    //     sortState.sort(function (a, b) {
-    //         console.log(a.name.last);
-    //         if (a.name.last > b.name.last) {
-    //           return -1;
-    //         }
-    //         if (a.name.last < b.name.last) {
-    //           return 1;
-    //         }
-    //         // names must be equal
-    //         return 0;
-    //     })
-    //     // setSortState([...sortState, users]);
-    //     setSortState(users);
-    //     console.log(setSortState);
-    // }
 
-    const handleSort = (event) => {
-        event.preventDefault();
+// function handleSort() {
+//     sortBy.sort(function(a,b){
+//         if (a.name.last > b.name.last) {
+//             return -1;
+//         }
+//         if (b.name.last > a.name.last) {
+//             return 1;
+//         }
+//         return 0;
+//     })
+//     setSortBy(users);
+// }
 
-        sortState.sort(function (a, b) {
-            console.log(b.name.last);
-            if (a.name.last > b.name.last) {
-              return -1;
-            }
-            if (a.name.last < b.name.last) {
-              return 1;
-            }
-            // names must be equal
-            return 0;
-        })
-        // setSortState([...sortState, users]);
-        setSortState(users);
-        // console.log(sortState);
-    }
- 
-    //compare function for tel #
+const handleFilterChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setFilterOption(
+        {
+            ...filterOption,
+            [name]: value
+        }
+    )
+    console.log(value)
 
-    const handleFilterChange = (event) => {
-        event.preventDefault();
-        const { name, value } = event.target;
-        setFilterOption(
-            {
-                ...filterOption,
-                [name]: value
-            }
-        )
-        console.log(value)
-
-        for (let i = 0; i < users.length; i++) {
-            if (value === users[i].name.first.toLowerCase() || value === users[i].name.last.toLowerCase()
-                || value === users[i].name.first || value === users[i].name.last) {
-                setFilteredState([...filteredState, users[i]
-                ])
-                console.log("Found employee")
-            }
-            else if (value === "") {
-                setFilteredState([])
-                console.log("Employee not found")
-            }
+    for (let i = 0; i < users.length; i++) {
+        if (value === users[i].name.first.toLowerCase() || value === users[i].name.last.toLowerCase()
+            || value === users[i].name.first || value === users[i].name.last) {
+            setFilteredState([...filteredState, users[i]
+            ])
+            console.log("Found employee")
+        }
+        else if (value === "") {
+            setFilteredState([])
+            console.log("Employee not found")
         }
     }
+}
 
-
-    return (
-        <>
-            <div id="container" className="container-fluid">
-                <div className="row">
-                    <div id="searchRow" className="col-12">
-                        <SearchBar
-                            handleFilterChange={handleFilterChange}
-                        />
-                    </div>
-                </div>
-                <div id="tableRow" className="row">
-                    <div className="col-12">
-                        <SearchResult
-                            users={filteredState.length > 0 ? filteredState : users}
-                            handleSort={handleSort}
-                        />
-                    </div>
+return (
+    <>
+        <div id="container" className="container-fluid">
+            <div className="row">
+                <div id="searchRow" className="col-12">
+                    <SearchBar
+                        handleFilterChange={handleFilterChange}
+                    />
                 </div>
             </div>
-        </>
-    )
+            <div id="tableRow" className="row">
+                <div className="col-12">
+                    <SearchResult
+                        users={filteredState.length > 0 ? filteredState : users}
+                        // handleSort={handleSort}
+                    />
+                </div>
+            </div>
+        </div>
+    </>
+)
 }
 
 export default ResultContainer;
