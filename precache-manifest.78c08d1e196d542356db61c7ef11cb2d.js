03c312b315749db6a92f776e0d0ceb2339bb0c9b
Laureni Wilkinson <las348@nyu.edu>
2020-08-03 19:27:41 -0400

    Updates

---
+++ precache-manifest.78c08d1e196d542356db61c7ef11cb2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de3379747d2e9a76cf186a8c40b5c43",
+    "url": "/tass/index.html"
+  },
+  {
+    "revision": "dd836c04e605805dcec9",
+    "url": "/tass/static/css/main.b53c170d.chunk.css"
+  },
+  {
+    "revision": "014340bc53ef28f629a3",
+    "url": "/tass/static/js/2.1d442fd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tass/static/js/2.1d442fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd836c04e605805dcec9",
+    "url": "/tass/static/js/main.53b00dce.chunk.js"
+  },
+  {
+    "revision": "b22de2da19275df535d8",
+    "url": "/tass/static/js/runtime-main.2c605151.js"
+  }
+]);
